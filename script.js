34551346ab4d6fd5c68fc550c451525e45d19a4d
myradio <154856702+pristaaaa@users.noreply.github.com>
2024-02-03 21:32:23 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function getMPDFile() {
+  return "https://dcqkiaqc3sngm.cloudfront.net/CH5/masterCH5.mpd";
+}
+
+function getClearKey() {
+  return "4b3898bc8a6ffd8b5cb34a7cd906dc35";
+}
+
+function getClearKeyId() {
+  return "9fbffd1ff21340169a5a1142bcf4ddad";
+}
